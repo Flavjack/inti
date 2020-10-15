e5689e60e9ed52944659bc511ab96c0cdcdf1269
Flavjack <flavjack@gmail.com>
2020-10-15 12:52:22 +0200

    include auto dep

--- inst/tarpuy/setup.r
+++ inst/tarpuy/setup.r
@@ -19,24 +19,20 @@ cran <- c("devtools"
          )
 
 git <- c(
-  "rstudio/sass"
-  , "rstudio/htmltools"
-  , "rstudio/bootstraplib"
+  "rstudio/bootstraplib"
   , "Flavjack/inti" # Tools and Statistical Procedures in Plant Science
   )
 
 installed <- c(cran, sub(".*/", "", git)) %in% rownames(installed.packages())
 
 if (any(installed == FALSE)) {
-
   cran_missing <- cran %in% c(cran, sub(".*/", "", git))[!installed == TRUE]
   cran_install <- c(cran, sub(".*/", "", git))[cran_missing == TRUE]
   install.packages( cran_install )
-
-}
+  }
 
 invisible(lapply(sub(".*/", "", git), unloadNamespace))
-invisible(lapply(git, devtools::install_github))
+invisible(lapply(git, devtools::install_github, dependencies = T))
 invisible(lapply(c(cran, sub(".*/", "", git)), library, character.only = TRUE))
 rm(cran, git, installed)
 
