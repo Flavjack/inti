3e3cea8d08962f5119bc3a763a3e1af155c9ac52
Flavio <flavjack@gmail.com>
2021-09-07 23:36:42 +0200

    cran fix

--- inst/yupana/server.R
+++ inst/yupana/server.R
@@ -4,7 +4,7 @@
 #> open https://flavjack.github.io/inti/
 #> open https://flavjack.shinyapps.io/yupanapro/
 #> author .: Flavio Lozano-Isla (lozanoisla.com)
-#> date .: 2021-08-07
+#> date .: 2021-09-07
 # -------------------------------------------------------------------------
 
 # -------------------------------------------------------------------------
