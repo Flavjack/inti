1c29b74d9c829adf93d8dbc742a53ef10d614f9b
Flavjack <flavjack@gmail.com>
2021-11-09 21:50:44 +0100

    updt logos

--- pkgdown/favicon/logo.R
+++ pkgdown/favicon/logo.R
@@ -1,44 +1,58 @@
-library(hexSticker)
-library(magick)
-library(showtext)
-library(dplyr)
-
-# open https://fonts.google.com/
-# font poma
-
 # -------------------------------------------------------------------------
 # inkaverse ---------------------------------------------------------------
 # -------------------------------------------------------------------------
 
-logo <- list.files("pkgdown/favicon/img"
-                   , full.names = T
-                   , pattern = "logo_inkaverse.jpg") %>% 
-  image_read() 
+library(huito)
+library(tidyverse)
 
-sticker(subplot = logo
-        , white_around_sticker = TRUE
-        , s_x = 1.0
-        , s_y = 0.81
-        , s_width = 1.7
-        , s_height = 1.7
-        , package = ""
-        , h_color = "#6b594b"
-        , h_fill = "#ffe701"
-        , h_size = 1
-        , url = "inkaverse.com"
-        , u_size = 8
-        , u_angle = 90
-        , u_x = 1.8
-        , u_y = 0.7
-        , filename = "pkgdown/favicon/img/inkaverse.png"
-        )
+#> open https://github.com/terinjokes/StickersStandard
 
 logo <- list.files("pkgdown/favicon/img"
                    , full.names = T
-                   , pattern = "inkaverse.png") %>%
-  image_read()  %>% 
-  image_transparent('white') %>% 
+                   , pattern = "png|jpg") 
+
+# huito_fonts()
+
+label <- label_layout(size = c(5.08, 5.08)
+                      , border_color = NA
+                      , border_width = 0
+                      , background = "#ffe701"
+                      ) %>% 
+  include_image(value = "pkgdown/favicon/img/logo_inkaverse.jpg" 
+                , size = c(3.97, 3.97)
+                , position = c(2.55, 2.06)
+                ) %>%
+  include_shape(size = 4.1
+                , border_width = 3
+                , border_color = "#505456"
+                , margin = -0.8
+                , position = c(2.54, 2.54)
+                , panel_color = "pink"
+                , panel_size = 5.08
+                ) %>%
+  include_text(value = "inkaverse.com"
+               , size = 6
+               , position = c(3.6, 0.75)
+               , angle = 30
+               , color = "white") %>%
+  label_print(filename = "pkgdown/favicon/img/inkaverse"
+              , margin = 0
+              , paper = c(5.5, 5.5)
+              , viewer = T
+              , smpres = 200
+              , mode = "c"
+              )
+
+logo <- list.files("pkgdown/favicon/img/"
+                   , full.names = T
+                   , pattern = "inkaverse.pdf") %>%
+  image_read_pdf()  %>% 
+  image_crop(geometry = "600x600+40") %>% 
+  image_crop(geometry = "560x600-40") %>% 
+  image_transparent('pink') %>% 
   image_write("pkgdown/favicon/img/inkaverse.png")
+
+# open man/figures/logo.png
 
 #> Fechas en el quipo
 
@@ -52,42 +66,45 @@ logo <- list.files("pkgdown/favicon/img"
 # inti --------------------------------------------------------------------
 # -------------------------------------------------------------------------
 
-logo <- list.files("pkgdown/favicon/img"
-                   , full.names = T
-                   , pattern = "logo_inti.jpg"
-                   ) %>% 
-  image_read()
+label <- label_layout(size = c(5.08, 5.08)
+                      , border_color = NA
+                      , border_width = 0
+                      , background = "#ffe701"
+) %>% 
+  include_image(value = "pkgdown/favicon/img/logo_inti.jpg" 
+                , size = c(3.97, 3.97)
+                , position = c(2.54, 2.35)
+  ) %>%
+  include_shape(size = 4.1
+                , border_width = 3
+                , border_color = "#505456"
+                , margin = -0.8
+                , position = c(2.54, 2.54)
+                , panel_color = "pink"
+                , panel_size = 5.08
+  ) %>%
+  include_text(value = "inkaverse.com"
+               , size = 6
+               , position = c(3.6, 0.75)
+               , angle = 30
+               , color = "white") %>%
+  label_print(filename = "pkgdown/favicon/img/inti"
+              , margin = 0
+              , paper = c(5.5, 5.5)
+              , viewer = T
+              , smpres = 200
+              , mode = "c"
+              )
 
-sticker(subplot = logo
-        , white_around_sticker = TRUE
-        , s_x = 1.0
-        , s_y = 0.90
-        , s_width = 1.7
-        , s_height = 1.7
-        , package = ""
-        , p_color = "red"
-        , p_size = 15
-        , h_color = "#6b594b"
-        , h_fill = "#ffe701"
-        , h_size = 1
-        , url = "inkaverse.com"
-        , u_size = 8
-        , u_angle = 90
-        , u_x = 1.8
-        , u_y = 0.7
-        , filename = "pkgdown/favicon/img/inti.png"
-        ) 
-
-logo <- list.files("pkgdown/favicon/img"
+logo <- list.files("pkgdown/favicon/img/"
                    , full.names = T
-                   , pattern = "inti.png") %>%
-  image_read()  %>% 
-  image_transparent('white') %>% 
+                   , pattern = "inti.pdf") %>%
+  image_read_pdf()  %>% 
+  image_crop(geometry = "600x600+40") %>% 
+  image_crop(geometry = "560x600-40") %>% 
+  image_transparent('pink') %>% 
+  # image_write("man/figures/logo.png")
   image_write("pkgdown/favicon/img/inti.png")
-
-file.copy(from = "pkgdown/favicon/img/inti.png"
-          , to = "man/figures/logo.png"
-          , overwrite = T)
 
 # -------------------------------------------------------------------------
 # yupana ------------------------------------------------------------------
